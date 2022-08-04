f3c1d5ac24079231b690dcbc6b76e4ae96b9cc8d
Andy-dev1 <andre.andre.jazz@gmail.com>
2022-08-03 21:43:21 -0300

    Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Detail from "./pages/Detail";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const App=()=>{
   return(
@@ -9,6 +10,7 @@ const App=()=>{
         <Route path="/" element={<Home/>} />
         <Route path="/:name" element={<Detail/>} />
       </Routes>
+      <GlobalStyles/>
     </Router>
   );
 }
