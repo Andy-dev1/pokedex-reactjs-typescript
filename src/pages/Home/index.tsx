a011e0c980087c0d51ac3d853600eee0bfe8535f
Andy-dev1 <andre.andre.jazz@gmail.com>
2022-08-30 01:48:39 -0300

    React Query Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import PokemonThumbnail from "../../components/PokemonThumbnail";
 import axios from 'axios';
 import { Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap';
-import {Container} from './styles'
+import { Container } from './styles'
 import { useQuery } from "react-query";
 
 interface ISpritesDreamWorldFrontDefault {
@@ -19,37 +19,67 @@ interface IPokemon {
   types: [Itype]
 }
 
+
+export const PokeFilter = () => {
+  return (<>
+
+  </>);
+}
+
 function Home() {
-  const [allPokemons, setAllPokemons] = useState<Array<IPokemon>>([]);
-  const [loadMore, setLoadMore] = useState<string>('https://pokeapi.co/api/v2/pokemon?limit=20')
+  
+  const [currentLimit, setCurrentLimit] = useState(20);
+  const [allPokemons, setAllPokemons] = useState<any>([]);
   const [search, setSearch] = useState('');
 
-  
-
-  const getAllPokemons = async () => {
-    const data = await axios.get(loadMore).then((response) => response.data).catch(e => console.log(e));
-
-    setLoadMore(data.next)
-
-    const createPokemonObject = (result: Array<IPokemon>) => {
-      result.forEach(async (pokemon: IPokemon) => {
-        const data = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`).then(response => response.data).catch(e => console.log(e));
-        setAllPokemons(currentList => [...currentList, data])
-      })
-    }
-
-    createPokemonObject(data.results)
+  const LoadMore = () => {
+   
+    refetch();
+    console.log(currentLimit);
+    setCurrentLimit(prev => prev + 20);
+    setAllPokemons(pokedata);
   }
+
+  const { data: pokedata, refetch } = useQuery<any>('Pokedata', async () => {
+    const { data: responseData } = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=' + currentLimit);
+
+    const mapUrl = responseData?.results.map((pokemon: IPokemon) => `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+
+    const response = axios.all(mapUrl.map((url: any) => axios.get(url)))
+      .then(axios.spread(function (...res) {
+        // all requests are now complete
+        return res;
+      }));
+
+    return response;
+
+  }, { refetchOnWindowFocus: false });
+
+  
+  useEffect(()=>{
+    setAllPokemons(pokedata);
+  },[pokedata])
+  
+  useMemo(()=>{
+    
+    LoadMore();
+  },[])
+
+
+
+  /* setLoadMore(response.data.next) */
+
+
   // eslint-disable-next-line
-  useEffect(() => { getAllPokemons() }, [])
+  //useEffect(() => { getAllPokemons() }, [])
 
   const filteredResult = useMemo(() => {
     if (search !== '') {
-      return allPokemons.filter((pokemon) => pokemon.name.toLowerCase().includes(search.toLowerCase()))
+      return allPokemons?.filter((pokemon: any) => pokemon.data.name.toLowerCase().includes(search.toLowerCase()))
     } else {
       return allPokemons;
     }
-  }, [search, allPokemons]);
+  }, [search, allPokemons, currentLimit,refetch,currentLimit]);
 
   return (
 
@@ -67,11 +97,11 @@ function Home() {
         </Col>
       </Row>
       <Row className="d-flex justify-content-center mt-5">
-        {filteredResult.sort((a, b) => a.id > b.id ? 1 : -1).map((pokemon: IPokemon, index: number) => <PokemonThumbnail id={pokemon.id} name={pokemon.name} sprites={pokemon.sprites.other.dream_world.front_default} types={pokemon.types[0].type.name} key={index} />)}
+        {filteredResult?.sort((a: any, b: any) => a.data.id > b.data.id ? 1 : -1).map((pokemon: any, index: number) => <PokemonThumbnail id={pokemon.data.id} name={pokemon.data.name} sprites={pokemon.data.sprites.other.dream_world.front_default} types={pokemon.data.types[0].type.name} key={index} />)}
 
       </Row>
       <Row className="d-flex justify-content-center mt-5 w-100">
-        <Button className="mt-5" onClick={() => getAllPokemons()}>Load More</Button>
+        <Button className="mt-5" onClick={LoadMore}>Load More</Button>
       </Row>
     </Container>
   );
